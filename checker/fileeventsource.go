36df721c345918f2ccae94d895fcdb43a6054445
Marc Widmer <mwidmer@protonmail.com>
2024-06-06 11:06:27 +0200

    fix my rebase messup (file source functions)

--- checker/fileeventsource.go
+++ checker/fileeventsource.go
@@ -1,19 +1,113 @@
 package checker
 
 import (
+	"fmt"
 	"os"
 	"slices"
 
 	"github.com/filecoin-project/mir/pkg/eventlog"
 	"github.com/filecoin-project/mir/pkg/pb/eventpb"
 	"github.com/filecoin-project/mir/pkg/pb/recordingpb"
+	"github.com/filecoin-project/mir/pkg/util/sliceutil"
+	"github.com/filecoin-project/mir/pkg/vcinterceptor/vectorclock"
 	"github.com/filecoin-project/mir/stdtypes"
 )
 
 // NOTE: if we deal with biiig logs, we should probably start streaming...
 // TODO: this is not general purpose, need a better way to decode serialized events
 
-func GetEventsFromFile(serializedDecoders []func(sEvt []byte) (stdtypes.Event, error), filenames ...string) []stdtypes.Event {
+func GetEventsFromFileSortedByTimestamp(serializedDecoders []func(sEvt []byte) (stdtypes.Event, error), filenames ...string) []stdtypes.Event {
+	recordingLog := getRecordingLog(filenames...)
+
+	// keeping original order of items
+	slices.SortStableFunc(recordingLog, func(a, b *recordingpb.Entry) int {
+		return int(a.GetTime() - b.GetTime())
+	})
+
+	eventLog := make([]stdtypes.Event, 0, len(recordingLog))
+	for _, rle := range recordingLog {
+		for _, e := range rle.Events {
+			var event stdtypes.Event = e
+			if serializedEvent, ok := event.(*eventpb.Event).Type.(*eventpb.Event_Serialized); ok {
+				for _, decoder := range serializedDecoders {
+					if decoded, err := decoder(serializedEvent.Serialized.GetData()); err == nil {
+						event = decoded
+						break
+					}
+				}
+			}
+			eventLog = append(eventLog, event)
+		}
+	}
+
+	return eventLog
+}
+
+func GetEventsFromFileSortedByVectorClock(serializedDecoders []func(sEvt []byte) (stdtypes.Event, error), filenames ...string) []stdtypes.Event {
+	recordingLog := getRecordingLog(filenames...)
+	type vcEvent struct {
+		vc    *vectorclock.VectorClock
+		event stdtypes.Event
+	}
+
+	vcEvents := make([]vcEvent, 0, len(recordingLog))
+	for _, rle := range recordingLog {
+		for _, e := range rle.Events {
+			var event stdtypes.Event = e
+			if serializedEvent, ok := event.(*eventpb.Event).Type.(*eventpb.Event_Serialized); ok {
+				for _, decoder := range serializedDecoders {
+					if decoded, err := decoder(serializedEvent.Serialized.GetData()); err == nil {
+						event = decoded
+						break
+					}
+				}
+			}
+			vcEvents = append(vcEvents, vcEvent{getVC(event), event})
+		}
+	}
+
+	vcEvents = sliceutil.TopologicalSortFunc(vcEvents, func(a, b vcEvent) bool {
+		return vectorclock.Less(a.vc, b.vc)
+	})
+
+	sliceutil.Filter(vcEvents, func(i int, t vcEvent) bool {
+		fmt.Printf("\n%v", t.vc)
+		if i > 0 {
+			cmp := vectorclock.Less(vcEvents[i-1].vc, t.vc)
+			if cmp {
+				fmt.Print("\t- GREATER")
+			} else {
+				fmt.Print("\t- NOT G")
+			}
+		}
+		return true
+	})
+
+	return sliceutil.Transform(vcEvents, func(_ int, vcE vcEvent) stdtypes.Event { return vcE.event })
+}
+
+func getVC(e stdtypes.Event) *vectorclock.VectorClock {
+	v, err := e.GetMetadata("vc")
+	if err != nil {
+		panic(err)
+	}
+	vc, ok := v.(map[string]interface{})
+	if !ok {
+		panic("fuck")
+	}
+	vcc, ok := vc["V"].(map[string]interface{})
+	if !ok {
+		panic("fuckfuck")
+	}
+	vcR := make(map[stdtypes.NodeID]uint32)
+	for k, v := range vcc {
+		vcR[stdtypes.NodeID(k)] = uint32(v.(float64))
+	}
+
+	return vectorclock.VectorClockFromMap(vcR)
+}
+
+func getRecordingLog(filenames ...string) []*recordingpb.Entry {
 	recordingLog := make([]*recordingpb.Entry, 0)
 
 	for _, fn := range filenames {
@@ -36,30 +130,5 @@ func GetEventsFromFile(serializedDecoders []func(sEvt []byte) (stdtypes.Event, e
 		recordingLog = append(recordingLog, entries...)
 	}
 
-	// actually, we want to interleave the histories
-	// for simplicity we join them and then sort them
-
-	// keeping original order of items
-	slices.SortStableFunc(recordingLog, func(a, b *recordingpb.Entry) int {
-		return int(a.GetTime() - b.GetTime())
-	})
-
-	eventLog := make([]stdtypes.Event, 0, len(recordingLog))
-	for _, rle := range recordingLog {
-		for _, e := range rle.Events {
-			var event stdtypes.Event = e
-			if serializedEvent, ok := event.(*eventpb.Event).Type.(*eventpb.Event_Serialized); ok {
-				for _, decoder := range serializedDecoders {
-					if decoded, err := decoder(serializedEvent.Serialized.GetData()); err == nil {
-						event = decoded
-						break
-					}
-				}
-			}
-			eventLog = append(eventLog, event)
-			// event.SetMetadata("node", stdtypes.NodeID(rle.NodeId))
-		}
-	}
-
-	return eventLog
+	return recordingLog
 }
